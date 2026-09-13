de60d84b66c58ceff3008f099705255fefebe20b
gatkuothchoap <gatkuothchop39@gmail.com>
2026-09-13 10:38:11 +0300

    Add files via upload

---
+++ plp-week1-python.py
@@ -0,0 +1,13 @@
+# Python project for name, age, goal and favorite language
+name = "Gatkuoth Chop"
+age = 28
+favorite_language = "Python"
+goal = "My goal is to be software engineer"
+
+print(f"My name is {name}")
+print(f"I am {age} years old")
+print(f"My favorite programming language is {favorite_language}")
+print(f"{goal}")
+# The Python script that introduce me to the audience
+introduction = "Hello. I am Gatkuoth Chop. A software engineer student at PLP"
+print(f"{introduction}")
